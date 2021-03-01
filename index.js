9fbb72eb2d8289651e234fb0ff64ff232d0c6ce4
sataller <56543871+sataller@users.noreply.github.com>
2021-03-01 21:12:59 +0300

    update 2

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const decryptButton = document.querySelector(".decryptButton");
 
 const edwardDecrypt = () => {
     const message = document.querySelector(".message").value.split("");
-    let result = message.map((item, index) => {
+   let result = message.map((item, index) => {
         if (item === message[index - 1] && item === message[index + 1]) {
-            return item
+            if (item !== counter) {
+                counter = item;
+                return item
+            }
+            counter = item;
         } else if (item !== message[index - 1] && item !== message[index + 1]) {
             return item
         }
